536c2827f0a85f837aef86ea6348d273698f77cc
Mario23leiva <mleiva@alumno.santoangel.org>
2023-04-17 11:14:23 +0200

    sdfg

---
+++ app/src/main/java/com/example/checkattablet/PantallaCarga.kt
@@ -0,0 +1,25 @@
+package com.example.checkattablet
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.navigation.Navigation.findNavController
+
+class PantallaCarga : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pantalla_carga)
+
+        Handler(Looper.myLooper()!!).postDelayed({
+
+            val intent = Intent(this, Login::class.java)
+            this.startActivity(intent)
+            this.overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+
+        }, 2500)
+
+
+    }
+}
